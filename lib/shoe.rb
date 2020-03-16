84812ddccd043dbd762b41e07a5511b508b25b84
Charles Butler <cdbutle@outlook.com>
2020-03-16 00:43:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+  def brand
+    @brand = brand
+  end
+  def brand
+    @brand
+  end
+  
+  attr_reader :color
+  attr_writer :color
+  attr_reader :size
+  attr_writer :size
+  attr_reader :material
+  attr_writer :material
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
+
+
+
+
 # Make your shoe class here!
